dc107f3a69c5b6a23e22707cf6c84dfbc1a4773c
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-14 09:40:01 +0530

    131222 recur

--- minFallPathSum.cpp
+++ minFallPathSum.cpp
@@ -6,6 +6,13 @@ int help(vector<vector<int>> &arr, int r, int c)
     int n = arr.size();
     if (r == n - 1)
         return arr[r][c];
+    int minn = INT_MAX;
+    for (int i = -1; i <= 1; i++)
+    {
+        if (c + i >= 0 && c + i < n)
+            minn = min(minn, arr[r][c] + help(arr, r + 1, c + i));
+    }
+    return minn;
 }
 
 int minFallingPathSum(vector<vector<int>> &arr)
@@ -17,9 +24,18 @@ int minFallingPathSum(vector<vector<int>> &arr)
     }
     return ans;
 }
-
 int main()
 {
-
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    cout << minFallingPathSum(arr);
     return 0;
 }
