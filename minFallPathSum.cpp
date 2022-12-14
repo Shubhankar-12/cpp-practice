ce0804ece1d64898e4f7f716b5bca88dfdb6ebf1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-14 09:53:00 +0530

    131222 recur memoized

--- minFallPathSum.cpp
+++ minFallPathSum.cpp
@@ -1,26 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int help(vector<vector<int>> &arr, int r, int c)
+int help(vector<vector<int>> &arr, int r, int c, vector<vector<int>> &dp)
 {
     int n = arr.size();
     if (r == n - 1)
         return arr[r][c];
+    if (dp[r][c] != -1)
+        return dp[r][c];
     int minn = INT_MAX;
     for (int i = -1; i <= 1; i++)
     {
         if (c + i >= 0 && c + i < n)
-            minn = min(minn, arr[r][c] + help(arr, r + 1, c + i));
+            minn = min(minn, arr[r][c] + help(arr, r + 1, c + i, dp));
     }
-    return minn;
+    return dp[r][c] = minn;
 }
 
 int minFallingPathSum(vector<vector<int>> &arr)
 {
     int ans = INT_MAX;
-    for (int i = 0; i < arr[0].size(); i++)
+    int n = arr.size();
+    vector<vector<int>> dp(n, vector<int>(n, -1));
+    for (int i = 0; i < n; i++)
     {
-        ans = min(ans, help(arr, 0, i));
+        ans = min(ans, help(arr, 0, i, dp));
     }
     return ans;
 }
