c4b1c2538b66b1db9d3879b5713877a57bca9ac8
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-15 22:11:36 +0530

    151222 tabular

--- LCS.cpp
+++ LCS.cpp
@@ -19,11 +19,28 @@ int LCS(string s1, string s2, vector<vector<int>> &dp)
     return dp[n][m];
 }
 
-int longestCommonSubsequence(string s1, string s2)
+int longestCommonSubsequence1(string s1, string s2)
 {
     int n = s1.size(), m = s2.size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
     return LCS(s1, s2, dp);
+}
+
+int longestCommonSubsequence(string s1, string s2)
+{
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[n - i] == s2[m - j])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[n][m];
 }
 
 int main()
