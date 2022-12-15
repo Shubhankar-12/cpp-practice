00acb1932ff4467db576859aecea7d333a041375
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2022-12-15 22:01:19 +0530

    151222 memoized

--- LCS.cpp
+++ LCS.cpp
@@ -1,18 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int longestCommonSubsequence(string s1, string s2)
+int LCS(string s1, string s2, vector<vector<int>> &dp)
 {
+    int n = s1.size(), m = s2.size();
     if (s1 == "" || s2 == "")
         return 0;
+    if (dp[n][m] != -1)
+        return dp[n][m];
     if (s1[0] == s2[0])
-        return 1 + longestCommonSubsequence(s1.substr(1), s2.substr(1));
+        dp[n][m] = 1 + LCS(s1.substr(1), s2.substr(1), dp);
     else
     {
-        int t1 = longestCommonSubsequence(s1, s2.substr(1));
-        int t2 = longestCommonSubsequence(s1.substr(1), s2);
-        return max(t1, t2);
+        int t1 = LCS(s1, s2.substr(1), dp);
+        int t2 = LCS(s1.substr(1), s2, dp);
+        dp[n][m] = max(t1, t2);
     }
+    return dp[n][m];
+}
+
+int longestCommonSubsequence(string s1, string s2)
+{
+    int n = s1.size(), m = s2.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+    return LCS(s1, s2, dp);
 }
 
 int main()
