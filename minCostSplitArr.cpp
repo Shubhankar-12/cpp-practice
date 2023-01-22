51591a2b64a3049d06ec0f4f90a35006e81034c1
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-22 09:33:45 +0530

    220123 q4

--- minCostSplitArr.cpp
+++ minCostSplitArr.cpp
@@ -12,14 +12,14 @@ int minCost(vector<int> &aur, int k)
     }
 
     vector<int> jl(aur.size() + 1);
-    for (int i = 1; i < aur.size(); i++)
+    for (int i = 1; i <= aur.size(); i++)
     {
         int cost = INT_MAX;
         for (int j = 0; j < i; j++)
         {
             if (dekh[aur[j]] > 1)
             {
-                cost = min(cost, jl[j] + +i - j);
+                cost = min(cost, jl[j] + k + i - j);
             }
         }
         jl[i] = cost;
