e8dc1b1111b4e8ee7819685ef19c31579a659c2e
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-22 09:30:29 +0530

    220123 q3

--- bitwiseStringEqual.cpp
+++ bitwiseStringEqual.cpp
@@ -1,6 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool makeStringsEqual(string s, string rtg)
+{
+    if (s == rtg)
+        return true;
+    int plh = 0, dkh = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '1')
+            plh += 1;
+        if (rtg[i] == '1')
+            dkh += 1;
+    }
+    if (plh >= 1 && dkh == 0)
+        return false;
+    if (plh == 0 && dkh >= 1)
+        return false;
+    return true;
+}
+
 int main()
 {
 
