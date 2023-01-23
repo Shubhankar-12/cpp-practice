3bea4becdf808ffa1ac8383bfcc60221dcb9061e
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-01-23 21:31:21 +0530

    230123

---
+++ findTownJudge.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findJudge(int n, vector<vector<int>> &arr)
+{
+    set<int> st;
+    for (auto i : arr)
+        st.insert(i[0]);
+    for (auto i : arr)
+    {
+        if (st.find(i[1]) == st.end())
+            return i[1];
+    }
+    return -1;
+}
+
+int main()
+{
+
+    return 0;
+}
